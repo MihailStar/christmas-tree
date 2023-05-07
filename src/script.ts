bd64814364c467a15b432c35b0a75226aae1987a
MihailStar <mhlstr@mail.ru>
2023-05-07 11:05:47 +0300

    feat: добавляет проект

---
+++ src/script.ts
@@ -0,0 +1,17 @@
+const numberOfIgnited = 7
+
+const visibleBranchs = document.querySelectorAll<HTMLDivElement>(
+  '.christmas-tree__branch:not(.christmas-tree__branch_invisible)'
+)
+
+setInterval(() => {
+  for (let index = 0; index < visibleBranchs.length; index += 1) {
+    visibleBranchs[index].classList.remove('christmas-tree__branch_ignited')
+  }
+
+  for (let index = 0; index < numberOfIgnited; index += 1) {
+    visibleBranchs[
+      Math.floor(Math.random() * visibleBranchs.length)
+    ].classList.add('christmas-tree__branch_ignited')
+  }
+}, 1000)
